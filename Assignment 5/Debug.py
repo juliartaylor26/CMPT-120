6327e31175f8a85d6ec8c49fce4039da827d9c83
juliartaylor26 <112398180+juliartaylor26@users.noreply.github.com>
2022-10-30 15:25:38 -0400

    i done

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -8,14 +8,14 @@ def main():
         
     #can you google and find what function you should use to check if it's numeric (an int?)?
     intInput = input("enter an int")
-    if intInput.?:
+    if intInput.isdigit:
         print("int!")
     else:
         print("not int :(")
     
     #what about if it's both letters and numbers?
     alphIntInput = input("Enter letters and numbers")
-    if alphIntInput.?:
+    if alphIntInput.isalnum:
         print("Letters and numbers!")
     else:
         print("weird characters :(")
@@ -26,13 +26,36 @@ def main():
         print("good!")
     else:
         print("not asterisk :(")
-        
-    #now write code to check if the input was either an asterisk OR an ampersand (&)
+
+
+    amperisk = input("Enter an asterisk [*] or an ampersand [&]")
+    if amperisk == "*" or amperisk == "&":
+        print("thanks!")
+    else:
+        print("darn")
+
+
+   #now write code to check if the input was either an asterisk OR an ampersand (&)
     
         
-    #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-    
+    #do the live example we did in class: ask user to input an integer, but before you cast it to an int,
+    #check that it's an integer before doing your variable = int(variable) command
+    wantInt = input("Enter an integer")
+    if wantInt.isdigit:
+        wantInt = int(wantInt)
+        print(wantInt)
+    else:
+        print("not an integer :(")
+
+
     # last challenge: find out how to check if the string input has the substring "marist"
     #google this one ;) substring is the key google term
-    
+    schoolName = input("Where do you go to school?")
+    substring = "marist"
+    if substring in schoolName:
+        print("me too!")
+    else:
+        print("lame")
+
+
 main()
