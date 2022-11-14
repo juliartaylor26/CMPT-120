277670086e0ffd199b4205d43145ad8d6f8d0e40
juliartaylor26 <112398180+juliartaylor26@users.noreply.github.com>
2022-11-14 17:27:41 -0500

    o_O

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -1,32 +1,39 @@
 class Dog:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 
 class Employee:
-    def __init__(self, name, idNumber, department):
+    def __init__(self, name, id_number, department):
         self.name = name
-        self.idNumber = idNumber
+        self.id_number = id_number
         self.department = department
-        
+
+
 class Cake:
-    #can you fill out the rest of this for me? im dumb
-    #the cake needs to have the cake flavor and cake frosting stored
-        
+    def __init__(self, cake_flavor, frosting_flavor):
+        self.name = name
+        self.cake_flavor = cake_flavor
+        self.frosting_flavor = frosting_flavor
+
+    #   can you fill out the rest of this for me? im dumb
+    #   the cake needs to have the cake flavor and cake frosting stored
+
+
 def main():
-    #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
+    #   fill this one out with a dog's name and age... can be your dog, friend's dog, etc
+    dog1 = Dog("Parker", 8)
     print(dog1.name, dog1.age)
-    
-    #and what about a new employee
-    newEmployee =
-    #how would we print out each of the variables from newEmployee?
-    print()
-    
-    #now create and print out a cake you make
-    
-    
-    
-    #and now create another cake and print it out
+
+    #   and what about a new employee
+    new_employee = Employee("Johnson", 54321, "Elevator Music")
+    #   how would we print out each of the variables from new_employee?
+    print(new_employee.name, new_employee.id_number, new_employee.department)
+
+    #   now create and print out a cake you make
+
+    # and now create another cake and print it out
+
 
 main()
